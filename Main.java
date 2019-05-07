e52b721b5d1c35ad8edf61993e63f22cfc881ade
Sandro <sandro.eberl@sulzer.de>
2019-05-07 13:33:47 +0200

    Test

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
 	public static void main(String... args){
 		System.out.println("Hello world");
 		System.out.println("Another hello world");
-		System.out.println("Another another hello world");
+		System.out.println("HihihihiAnother another hello world");
 	}
 
 }
