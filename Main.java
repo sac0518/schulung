a5dec448b822f2eb3b28815d4dd0fc8f5d61ad1f
Sandro <sandro.eberl@sulzer.de>
2019-05-07 13:30:37 +0200

    something

--- Main.java
+++ Main.java
@@ -2,5 +2,7 @@ public class Main {
 	public static void main(String... args){
 		System.out.println("Hello world");
 		System.out.println("Another hello world");
+		System.out.println("Another another hello world");
 	}
+
 }
