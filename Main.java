e3621a909001c4eb2de5c7b8c935a6a005209e68
Sandro <sandro.eberl@sulzer.de>
2019-05-07 14:42:02 +0200

    changed

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ public class Main {
 	public static void main(String... args){
 		System.out.println("Hello world");
 		System.out.println("Another hello world");
-		System.out.println("HihihihiAnother another hello world");
 
 }
